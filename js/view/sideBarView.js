d4fa3ab25ef5619f9eb24afa7ef08545bfb0ca8f
Ella Wirstad <ella.wg@hotmail.com>
2019-01-29 14:06:43 +0100

    fix hamburger menu

--- js/view/sideBarView.js
+++ js/view/sideBarView.js
@@ -22,5 +22,16 @@ var SideBarView = function (container, model) {
         model.setNumberOfGuests(newNumberOfGuest);
     })
 
+    $(document).ready(function() {
+
+        // Check for click events on the navbar burger icon
+        $(".navbar-burger").click(function() {
+      
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+      
+        });
+      });
 
 }
