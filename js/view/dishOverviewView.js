e714af67a47fee277bc72de10e18841f787e0ce9
lindacnatt <linda.cnattingius@gmail.com>
2019-01-30 15:42:29 +0100

    style overview

--- js/view/dishOverviewView.js
+++ js/view/dishOverviewView.js
@@ -1,6 +1,7 @@
 var DishOverviewView = function (container, model) {
 
     const numberOfGuests = container.find('#numberOfGuests');
+   
 
     numberOfGuests.html(model.getNumberOfGuests());
 
@@ -10,11 +11,16 @@ var DishOverviewView = function (container, model) {
                     .addClass("menuList");
 
     container.append($menuList);
+    
 
     menu.forEach(function (j) {
         if (j !== undefined){
             new DishItemView($(".menuList"), j, model);
+            
         }
+    
+    container.append($menuList);
     });
-    container.append('<p class="price">' + model.getTotalMenuPrice() +  ' SEK </p>');
+    container.append('<hr/> <p class="price">'+ 'Total: ' + model.getTotalMenuPrice() +  ' SEK </p>');
+    container.append('<button class="button recipeBtn"> Print Full Recipe </button>')
 }
