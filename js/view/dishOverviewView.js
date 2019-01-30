f2f695e1b3538f912a3ae14f04990a9c57add582
Ella Wirstad <ella.wg@hotmail.com>
2019-01-30 16:27:53 +0100

    fix dish overview prices

--- js/view/dishOverviewView.js
+++ js/view/dishOverviewView.js
@@ -1,26 +1,32 @@
 var DishOverviewView = function (container, model) {
 
     const numberOfGuests = container.find('#numberOfGuests');
-   
 
     numberOfGuests.html(model.getNumberOfGuests());
 
     const menu = model.getFullMenu();
 
-    var $menuList = $("<div/>")  
-                    .addClass("menuList");
+    var menuList = $("<div/>")
+        .addClass("menuList");
 
-    container.append($menuList);
-    
+    var overView = $("<div/>")
+        .addClass("overView");
+    menuList.append(overView);
+
+    container.append(menuList);
 
     menu.forEach(function (j) {
-        if (j !== undefined){
-            new DishItemView($(".menuList"), j, model);
-            
+        if (j !== undefined) {
+
+            new DishItemView($(".overView"), j, model);
+            var dishPrice = $("<div/>")
+                .addClass("dishPrice")
+            overView.append(dishPrice)
+            dishPrice.append(model.getTotalDishPrice(j.id) + ' SEK');
         }
-    
-    container.append($menuList);
+
+        container.append(menuList);
     });
-    container.append('<hr/> <p class="price">'+ 'Total: ' + model.getTotalMenuPrice() +  ' SEK </p>');
+    container.append('<hr/> <p class="price">' + 'Total: ' + model.getTotalMenuPrice() + ' SEK </p>');
     container.append('<button class="button recipeBtn"> Print Full Recipe </button>')
 }
