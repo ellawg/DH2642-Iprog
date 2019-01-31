92036866c90b788b7ba54fa1fe588c844a079eeb
Ella Wirstad <ella.wg@hotmail.com>
2019-01-31 17:20:29 +0100

    add observer on numberofguests

---
+++ js/controller/sideBarController.js
@@ -0,0 +1,13 @@
+var SideBarController = function (view, model) {
+
+    view.plusButton.on('click', function () {
+        const newNumberOfGuest = model.getNumberOfGuests() + 1;
+        model.setNumberOfGuests(newNumberOfGuest);
+    })
+
+    view.minusButton.on('click', function () {
+        const newNumberOfGuest = model.getNumberOfGuests() - 1;
+        model.setNumberOfGuests(newNumberOfGuest);
+    })
+
+}
