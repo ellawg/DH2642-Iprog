a6af33dac683f2233a7e2da245fef99b29d1af04
lindacnatt <linda.cnattingius@gmail.com>
2019-02-04 22:22:15 +0100

    add styling and expand content to window

--- js/view/dishItemView.js
+++ js/view/dishItemView.js
@@ -1,11 +1,13 @@
 var DishItemView = function (container, dish, model, showPrice) {
     var dishDiv = $("<div/>")  
                  .addClass("dish")
-                 .addClass("column");
+                 .addClass("column box");
+                 
+                 
 
     $(container).append(dishDiv);
     dishDiv.append('<img src="images/' + dish.image +'"' + '/>');
-    dishDiv.append('<a class="button">' + dish.name + '</a>');
+    dishDiv.append('<a class="dishName">' + dish.name + '</a>');
     if (showPrice == true ){
         dishDiv.append('<p class="itemPrice">' + model.getTotalDishPrice(dish.id) + ' SEK</p>');
     }
