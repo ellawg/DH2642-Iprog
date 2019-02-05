c6300dbb1ed6cdf91a90f7a9a36d1cdc2240c83a
Ella Wirstad <ella.wg@hotmail.com>
2019-02-05 21:15:10 +0100

    move dish search logic to ctrl & change to lowercase

--- js/controller/dishSearchController.js
+++ js/controller/dishSearchController.js
@@ -1,4 +1,21 @@
-var DishSearchController = function (model) {
+var DishSearchController = function (view, model, app) {
+
+    view.searchBtn.on('click', function () {
+        view.dishList.empty();
+        const typeVal = document.getElementById('typeOption').value;
+        const inputVal = document.getElementById('searchInput').value;
+        const dishes = model.getAllDishes(typeVal, inputVal);
+        if (dishes.length == 0) {
+            view.dishList.append('<p>Could not find any dishes</p>')
+        }
+        dishes.forEach(function (j) {
+            new DishItemView($(".dishList"), j, model, false, app);
+        });
+    })
+
+
+
+
     
 
 
--- js/view/dishSearchView.js
+++ js/view/dishSearchView.js
@@ -5,35 +5,12 @@ var DishSearchView = function (container, model, app) {
     findDish.append('<input class="input" id="searchInput" type="text" placeholder="Enter key words" value=""></input>');
     findDish.append('<form class="select"><select id="typeOption"><option value="all">All</option> <option value="starter">Starter</option><option value="main dish">Main dish</option><option value="dessert">Dessert</option></select></form><button class="button" id="searchBtn">Search</button>')
 
-    const searchBtn = container.find('#searchBtn');
+    this.searchBtn = container.find('#searchBtn');
 
-    var dishList = $("<div/>")
+    this.dishList = $("<div/>")
         .addClass("dishList");
 
-    container.append(dishList);
-
-    searchBtn.on('click', function () {
-        dishList.empty();
-        const typeVal = document.getElementById('typeOption').value;
-        const inputVal = document.getElementById('searchInput').value;
-        console.log(inputVal);
-        const dishes = model.getAllDishes(typeVal, inputVal);
-        if (dishes.length == 0) {
-            dishList.append('<p>Could not find any dishes</p>')
-        }
-        dishes.forEach(function (j) {
-            new DishItemView($(".dishList"), j, model, false, app);
-        });
-    })
-
-
-
-
-    //const dishes = model.getEveryDish();
-
-
-
-
+    container.append(this.dishList);
 
 
 }
