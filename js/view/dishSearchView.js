fa814f438d04ff2208b9438761aa40d4fe50db77
Ella Wirstad <ella.wg@hotmail.com>
2019-02-05 14:00:23 +0100

    put search html in view

--- js/view/dishSearchView.js
+++ js/view/dishSearchView.js
@@ -1,11 +1,16 @@
 var DishSearchView = function (container, model, app) {
-    
+    const findDish = container.find('.findDish');
+
+    findDish.append('<h2>Find a dish</h2>');
+    findDish.append('<input class="input" id="searchInput" type="text" placeholder="Enter key words"></input>');
+    findDish.append(' <div class="select"><select><option>All</option> <option>Starter</option><option>Main dish</option><option>Dessert</option></select></div><a class="button" id="searchBtn">Search</a>')
+
     const dishes = model.getEveryDish();
 
-    var $dishList = $("<div/>")  
+    var dishList = $("<div/>")  
                     .addClass("dishList");
 
-    container.append($dishList);
+    container.append(dishList);
     
     dishes.forEach(function (j) {
         new DishItemView($(".dishList"),j, model, false, app);
