381cee40f18ddae36f7824e9362e1c39e27feed0
Ella Wirstad <ella.wg@hotmail.com>
2019-02-18 10:28:48 +0100

    fix dish details loading icon

--- js/view/dishDetailsView.js
+++ js/view/dishDetailsView.js
@@ -5,26 +5,36 @@ var DishDetailsView = function (container, model) {
     const specs = container.find('.specs');
 
 
+    const loader = $("<div/>").addClass("loader");
+    container.append(loader);
+
+
     var updates = () => {
+        dishInfo.hide();
+        dishSpecs.hide();
+        loader.show();
         specs.empty();
         dishInfo.empty();
 
         this.id = model.getDishId();
-        
-        model.getDish(this.id).then(dish => { 
-            dishInfo.append('<h1 class="dishName">' + dish.title + '</h1>'); 
-            dishInfo.append('<img src="' + dish.image + '"' +'/>');
+
+        model.getDish(this.id).then(dish => {
+            dishInfo.append('<h1 class="dishName">' + dish.title + '</h1>');
+            dishInfo.append('<img src="' + dish.image + '"' + '/>');
             dishInfo.append('<p>' + dish.instructions + '</p>');
 
             specs.append('Ingredients for ' + numberOfGuests + ' people');
 
-            dish.extendedIngredients.forEach((item) =>{
+            dish.extendedIngredients.forEach((item) => {
                 specs.append('<tr>');
                 specs.append('<td>' + Math.ceil(item.quantity) * numberOfGuests + ' ' + item.unit + '</td>');
                 specs.append('<td>' + item.name + '</td>');
                 specs.append('<td>' + 'SEK' + ' ' + item.amount * numberOfGuests + '</td>');
                 specs.append('<tr>');
             })
+            dishInfo.show();
+            dishSpecs.show();
+            loader.hide();
         }).catch(error => {
             console.log(error);
         });
